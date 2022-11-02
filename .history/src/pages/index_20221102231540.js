8bc3fb44f9013ad79983018fd45771f7b1cbd80e
Anna Maguire <anna.maguire103@gmail.com>
2022-11-02 23:17:23 +0000

    update

---
+++ .history/src/pages/index_20221102231540.js
@@ -0,0 +1,149 @@
+import * as React from 'react';
+
+import AttributeGrid from '../components/AttributeGrid';
+import Container from '../components/Container';
+import Hero from '../components/Hero';
+import BlogPreviewGrid from '../components/BlogPreviewGrid';
+import Highlight from '../components/Highlight';
+import Layout from '../components/Layout/Layout';
+import ProductCollectionGrid from '../components/ProductCollectionGrid';
+import ProductCardGrid from '../components/ProductCardGrid';
+import Quote from '../components/Quote';
+import Title from '../components/Title';
+
+import { generateMockBlogData, generateMockProductData } from '../helpers/mock';
+
+import * as styles from './index.module.css';
+import { Link, navigate } from 'gatsby';
+
+const IndexPage = () => {
+  const newArrivals = generateMockProductData(3, 'shirt');
+  const blogData = generateMockBlogData(3);
+
+  const goToShop = () => {
+    navigate('/shop');
+  };
+
+  return (
+    <Layout disablePaddingBottom>
+      {/* Hero Container */}
+      <Hero
+        maxWidth={'500px'}
+        image={'/jumperBasket.JPG'}
+        title={'New Collection'}
+        subtitle={'Organic Baby and Toddler Clothing'}
+        ctaText={'shop now'}
+        ctaAction={goToShop}
+      />
+
+      {/* Message Container */}
+      <div className={styles.messageContainer}>
+        <p>
+          All of our clothing is thoughtfully crafted using sustainably sourced raw materials
+        </p>
+        <p>
+        knit by local women in{' '}
+          <span className={styles.gold}>Ndaragwa Kenya </span>
+        </p>
+      </div>
+
+      {/* Collection Container */}
+      <div className={styles.collectionContainer}>
+        <Container size={'large'}>
+          <Title name={'New Collection'} />
+          <ProductCollectionGrid />
+        </Container>
+      </div>
+
+      {/* New Arrivals */}
+      <div className={styles.newArrivalsContainer}>
+        <Container>
+          <Title name={'New Arrivals'} link={'/shop'} textLink={'view all'} />
+          <ProductCardGrid
+            spacing={true}
+            showSlider
+            height={480}
+            columns={3}
+            data={newArrivals}
+          />
+        </Container>
+      </div>
+
+      {/* Highlight  */}
+      <div className={styles.highlightContainer}>
+        <Container size={'large'} fullMobile>
+          <Highlight
+            image={'/giraffesWall.jpg'}
+            altImage={'highlight image'}
+            miniImage={'/DandHat.jpg'}
+            miniImageAlt={'mini highlight image'}
+            title={'Organic Knitwear'}
+            description={`We source our organic cotton and hand-spun wool from Kenana Knitters who are based in Njoro in the
+            Kenyan Rift Valley. Our objective is to contribute to creating sustainable livelihoods in rural Kenya by
+            tapping into the talent of our local women to make beautiful hand-knit garments using sustainable local
+            materials.`}
+            textLink={'shop now'}
+            link={'/shop'}
+          />
+        </Container>
+      </div>
+
+      {/* Promotion */}
+      {/* <div className={styles.promotionContainer}>
+        <Hero image={'/banner2.png'} title={`-50% off \n All Essentials`} />
+        <div className={styles.linkContainers}>
+          <Link to={'/shop'}>WOMAN</Link>
+          <Link to={'/shop'}>MAN</Link>
+        </div>
+      </div> */}
+
+      {/* Quote */}
+      <Quote
+        bgColor={'var(--standard-light-grey)'}
+        title={'About Ann & Betty'}
+        quote={
+          '“ Kind to delicate skin and kind to our planet ”'
+        }
+      />
+
+      {/* Blog Grid */}
+      <div className={styles.blogsContainer}>
+        <Container size={'large'}>
+          <Title name={'Blog'} subtitle={'Notes on life and style'} />
+          <BlogPreviewGrid data={blogData} />
+        </Container>
+      </div>
+
+      {/* Promotion */}
+      <div className={styles.sustainableContainer}>
+        <Hero
+          image={'/knitters.jpg'}
+          title={'We are Sustainable'}
+          subtitle={
+            'From caring for our land to supporting our people, discover the steps we’re taking to do more for the world around us.'
+          }
+          ctaText={'read more'}
+          maxWidth={'660px'}
+          ctaStyle={styles.ctaCustomButton}
+        />
+      </div>
+
+      {/* Social Media */}
+      <div className={styles.socialContainer}>
+        <Title
+          name={'Styled by You'}
+          subtitle={'Tag @sydney to be featured.'}
+        />
+        <div className={styles.socialContentGrid}>
+          <img src={`/social/socialMedia1.png`} alt={'social media 1'} />
+          <img src={`/social/socialMedia2.png`} alt={'social media 2'} />
+          <img src={`/social/socialMedia3.png`} alt={'social media 3'} />
+          <img src={`/social/socialMedia4.png`} alt={'social media 4'} />
+        </div>
+      </div>
+      <AttributeGrid />
+    </Layout>
+  );
+};
+
+export default IndexPage;
